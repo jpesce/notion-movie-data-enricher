b3db309def89dc811b638143153acb8026a43409
João Pesce <jpesce@gmail.com>
2025-08-14 18:02:11 -0300

    fix: update IMDB API
    Migrate to new IMDB API as old one no longer functions

--- lib/imdb.rb
+++ lib/imdb.rb
@@ -6,23 +6,13 @@ require_relative "../util/extend_net"
 module IMDB
   include Logging
 
+  REST_ENDPOINT = "https://api.imdbapi.dev/"
   GRAPHQL_ENDPOINT = "https://graph.imdbapi.dev/v1"
 
   def self.get_rating(imdb_id)
-    query = <<~GRAPHQL
-      {
-        title(id: "#{imdb_id}") {
-          rating {
-            aggregate_rating
-          }
-        }
-      }
-    GRAPHQL
-
-    uri = URI(GRAPHQL_ENDPOINT)
+    uri = URI("#{REST_ENDPOINT}titles/#{imdb_id}")
     request = Net::HTTP::Post.new(uri)
     request["Content-Type"] = "application/json"
-    request.body = { query: query }.to_json
 
     logger.info("Querying IMDb for rating of #{imdb_id} - REQUEST - " + request.to_json)
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
@@ -31,7 +21,7 @@ module IMDB
     logger.info("Querying IMDb for rating of #{imdb_id} - RESPONSE - " + response.to_json)
 
     if response.is_a?(Net::HTTPSuccess)
-      return JSON.parse(response.body)["data"]["title"]["rating"]["aggregate_rating"]
+      return JSON.parse(response.body)["rating"]["aggregate_rating"]
     end
 
     nil
